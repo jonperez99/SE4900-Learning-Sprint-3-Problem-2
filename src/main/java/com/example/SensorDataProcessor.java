790a137df2cb7eefea3f77664cf6c2627d436073
jonperez99 <45044054+jonperez99@users.noreply.github.com>
2026-03-26 17:43:08 -0500

    made 5 changes to sensordataprocessor calculate

--- src/main/java/com/example/SensorDataProcessor.java
+++ src/main/java/com/example/SensorDataProcessor.java
@@ -3,8 +3,7 @@ package com.example;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
-
-public class SensorDataProcessor{
+public class SensorDataProcessor {
 
     // Senson data and limits.
     public double[][][] data;
@@ -28,10 +27,41 @@ public class SensorDataProcessor{
     }
 
     // calculate data
+
+    /*
+     * manual improvements to the original
+     * 
+     * 1. put iLength, jLength, kLength as variables to reduce the amount of times
+     * .length is called
+     * 
+     * 2. took away math.pow statements in the math.abs comparisons, comparisons
+     * would be the same if they were removed
+     * 
+     * 3. average(data2[i][j]) is called multiple times, put it into a variable to
+     * reduce times it is called.
+     * 
+     * 4. Else continue is redundant, removed it.
+     * 
+     * 5. Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k] can be simplified
+     * to data2[i][j][k] > data[i][j][k]:
+     * 
+     * | Math.max(x, y) > x | y > x
+     * -------------------------------------------------------------------
+     * y < x | false | false
+     * | |
+     * y == x | false | false
+     * | |
+     * y > x | true | true
+     * 
+     */
     public void calculate(double d) {
 
         int i, j, k = 0;
-        double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
+        int iLength = data.length;
+        int jLength = data[0].length;
+        int kLength = data[0][0].length;
+
+        double[][][] data2 = new double[iLength][jLength][kLength];
 
         BufferedWriter out;
 
@@ -39,20 +69,20 @@ public class SensorDataProcessor{
         try {
             out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
 
-            for (i = 0; i < data.length; i++) {
-                for (j = 0; j < data[0].length; j++) {
-                    for (k = 0; k < data[0][0].length; k++) {
+            for (i = 0; i < iLength; i++) {
+                for (j = 0; j < jLength; j++) {
+                    for (k = 0; k < kLength; k++) {
                         data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
 
-                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+                        double averageData2 = average(data2[i][j]);
+
+                        if (averageData2 > 10 && averageData2 < 50)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                        else if (data2[i][j][k] > data[i][j][k])
                             break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                        else if (Math.abs(data[i][j][k]) < Math.abs(data2[i][j][k])
                                 && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
                             data2[i][j][k] *= 2;
-                        else
-                            continue;
                     }
                 }
             }
@@ -69,5 +99,5 @@ public class SensorDataProcessor{
             System.out.println("Error= " + e);
         }
     }
-    
+
 }
